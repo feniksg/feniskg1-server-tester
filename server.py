21ce2d2b52585964832e368a37286fd7a70b0d71
feniksG <doroxincavely@gmail.com>
2023-05-20 10:15:59 +0300

    test_html 1

---
+++ server.py
@@ -0,0 +1,9 @@
+﻿from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/',methods=['GET'])
+def index():
+    return render_template("index.html")
+
+app.run(debug = True,port=8000)
